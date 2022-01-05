67f341dee14795fa49f62dbe44def49d165ff3f1
Chakyeoungho <86039781+Chakyeoungho@users.noreply.github.com>
2022-01-05 23:53:43 +0900

    update
    update

--- Minesweeper/Constant.h
+++ Minesweeper/Constant.h
@@ -1,19 +1,19 @@
 ﻿#ifndef __CONSTANT_H__
 #define __CONSTANT_H__
 
-#define EASY_X_COUNT    9
-#define EASY_Y_COUNT    9
-#define NORMAL_X_COUNT 16
-#define NORMAL_Y_COUNT 16
-#define HARD_X_COUNT   30
-#define HARD_Y_COUNT   16
+#define EASY_X_COUNT    9    // 쉬움 난이도 x축 개수
+#define EASY_Y_COUNT    9    // 쉬움 난이도 y축 개수
+#define NORMAL_X_COUNT 16    // 보통 난이도 x축 개수
+#define NORMAL_Y_COUNT 16    // 보통 난이도 y축 개수
+#define HARD_X_COUNT   30    // 어려움 난이도 x축 개수
+#define HARD_Y_COUNT   16    // 어려움 난이도 y축 개수
 
-#define EASY_GRID_SIZE   40
-#define NORMAL_GRID_SIZE 35
-#define HARD_GRID_SIZE   30
+#define EASY_GRID_SIZE   40    // 쉬움 난이도 사각형 크기
+#define NORMAL_GRID_SIZE 35    // 보통 난이도 사각형 크기
+#define HARD_GRID_SIZE   30    // 어려움 난이도 사각형 크기
 
-#define EASY_MINE_NUM   (( 9) * ( 9))
-#define NORMAL_MINE_NUM ((16) * (16))
-#define HARD_MINE_NUM   ((30) * (16))
+#define EASY_MINE_NUM   (( 9) * ( 9))    // 쉬움 난이도 총 타일 개수
+#define NORMAL_MINE_NUM ((16) * (16))    // 보통 난이도 총 타일 개수
+#define HARD_MINE_NUM   ((30) * (16))    // 어려움 난이도 총 타일 개수
 
 #endif // !__CONSTANT_H__
