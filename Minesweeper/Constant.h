1ffb22e59e092028e0b18ff52c9563da9d19d01e
Chakyeoungho <86039781+Chakyeoungho@users.noreply.github.com>
2022-01-05 23:41:20 +0900

    update
    Constant.h

---
+++ Minesweeper/Constant.h
@@ -0,0 +1,19 @@
+﻿#ifndef __CONSTANT_H__
+#define __CONSTANT_H__
+
+#define EASY_X_COUNT    9
+#define EASY_Y_COUNT    9
+#define NORMAL_X_COUNT 16
+#define NORMAL_Y_COUNT 16
+#define HARD_X_COUNT   30
+#define HARD_Y_COUNT   16
+
+#define EASY_GRID_SIZE   40
+#define NORMAL_GRID_SIZE 35
+#define HARD_GRID_SIZE   30
+
+#define EASY_MINE_NUM   (( 9) * ( 9))
+#define NORMAL_MINE_NUM ((16) * (16))
+#define HARD_MINE_NUM   ((30) * (16))
+
+#endif // !__CONSTANT_H__
